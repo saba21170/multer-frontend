0719b7e618532f87c874659c7cab1a088951348a
Saba Saeed <Saba.Saeed@arhamsoft.org>
2024-01-02 11:13:56 +0500

    Initial commit

---
+++ src/components/User.js
@@ -0,0 +1,70 @@
+import React,{useState} from 'react';
+import './User.css'
+import axious from 'axios';
+
+const User = () => {
+    const [newUser,setNewUser] = useState(
+        {
+            name: '',
+            birthdate: '',
+            photo: '',
+
+        }
+    );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('photo', newUser.photo);
+        formData.append('birthdate', newUser.birthdate);
+        formData.append('name', newUser.name);
+
+        axious.post('http://localhost:5000/users/add/',formData)
+        .then(res =>{
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+         });
+
+
+
+    }
+    const handleChange = (e) =>{
+        setNewUser({...newUser, [e.target.name]: e.target.value});
+
+    }
+    const handlePhoto = (e) => {
+        setNewUser({...newUser, photo: e.target.files[0]});
+    }
+    return(
+        <form onSubmit={handleSubmit} encType='multipart/form-data'>
+            <input 
+                type="file" 
+                accept=".png, .jpg, .jpeg"
+                name="photo"
+                onChange={handlePhoto}
+            />
+
+            <input 
+                type="text"
+                placeholder="name"
+                name="name"
+                value={newUser.name}
+                onChange={handleChange}
+            />
+
+            <input 
+                type="date"
+                name="birthdate"
+                value={newUser.date}
+                onChange={handleChange}
+            />
+
+            <input 
+                type="submit"
+            />
+        </form>
+
+    );
+}
+export default User;
